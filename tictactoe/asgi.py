fd9ff9500261d99a89797d916c1271676a226cd1
jairo <jairo@JAIRO-PC>
2024-03-29 11:45:36 -0600

    Initial commit

---
+++ tictactoe/asgi.py
@@ -0,0 +1,19 @@
+## tic_tac_toe/asgi.py
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import game.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tictactoe.settings')
+
+# application = get_asgi_application()
+application = ProtocolTypeRouter({
+"http": get_asgi_application(),
+"websocket": AuthMiddlewareStack(
+URLRouter(
+game.routing.websocket_urlpatterns
+)
+),
+})
